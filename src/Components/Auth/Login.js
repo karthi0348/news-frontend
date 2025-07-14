ed3cbe5a758df9286487a2471b9c62a9ed5b70a0
karthi0348 <karthi0348@gmail.com>
2025-07-14 13:09:59 +0530

    commit

--- src/Components/Auth/Login.js
+++ src/Components/Auth/Login.js
@@ -2,7 +2,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import {API_URL} from '../../api'
-
 import { useNavigate, Link } from 'react-router-dom';
 
 
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,7 +1,7 @@
 // news-app-frontend/src/components/HomePage.js
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import {API_URL} from '../api'
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 
