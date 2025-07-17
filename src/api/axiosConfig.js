da0bf288cfe31c79264b00a1f2b213d6dbd9d069
karthi0348 <karthi0348@gmail.com>
2025-07-17 23:36:14 +0530

    commit newest

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API_BASE_URL = 'https://news-backend-jxwy.vercel.app';
+const API_BASE_URL = 'https://news-backend-nine-taupe.vercel.app/';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
