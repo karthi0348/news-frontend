a82bc695a8dc3f961bce62ce46c396c49bede833
karthi0348 <karthi0348@gmail.com>
2025-07-18 10:05:23 +0530

    new code

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API_BASE_URL = 'https://news-backend-nine-taupe.vercel.app/';
+const API_BASE_URL = 'https://bqhktxjd-8000.inc1.devtunnels.ms';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
